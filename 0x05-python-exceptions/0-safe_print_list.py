18193bc768a2d8fee462836064a5ebf14126718c
Bampascal <akinolabamemmanuel@gmail.com>
2023-01-23 22:49:17 +0000

    0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,4 @@
-#!/usr/bin/python3
-# 0-safe_print_list.py
-# Bamidele Emmanuel Akinola
-
-
-def safe_print_list(my_list=[], x=0):
+afe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
     Args:
         my_list (list): The list to print elements from.
